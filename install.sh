1178665fb732acd47310ce2c6b463e94e1c98e52
Hacker M3sr <64711640+HackerM3sr@users.noreply.github.com>
2020-06-16 16:03:46 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+apt update
+
+apt upgrade
+
+apt install python3
+
+apt install pip3
+
+pip3 install flask
+
+apt install openssh
+
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
